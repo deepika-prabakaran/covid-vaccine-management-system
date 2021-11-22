e8ac75b5110d230cf7a5e145dbd5c181437a5866
Deepika <87610680+deepika-prabakaran@users.noreply.github.com>
2021-11-22 12:53:24 +0530

    Update message1.py

--- message1.py
+++ message1.py
@@ -1,5 +1,5 @@
 import requests
-contact_no = 7339274549
+contact_no = 9988775544
 url = "https://www.fast2sms.com/dev/bulk"
 payload = f"sender_id=FSTSMS&message=Congrats! You took your 1st dose of vaccination on 21/11/2021 and your 2nd dose date is 03/01/2022&language=english&route=p&numbers={contact_no}"
 headers = {
