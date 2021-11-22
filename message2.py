f3659cd564721c015c34ed5ee2891b9869ad34ad
Deepika <87610680+deepika-prabakaran@users.noreply.github.com>
2021-11-22 12:25:20 +0530

    Create message2.py

---
+++ message2.py
@@ -0,0 +1,12 @@
+import requests
+contact_no = 9361338393
+url = "https://www.fast2sms.com/dev/bulk"
+payload = f"sender_id=FSTSMS&message=Congrats! You are sucessfully vaccinated with both the doses=english&route=p&numbers={contact_no}"
+headers = {
+'authorization': "Mefbs6CNUnpioIKrcLdDSxW8Q0OlEtaRuGzT4B3vYPyXmwFh2ZgDm8XkhvxCOjLV6edIcaP5tRTJnM07",
+'Content-Type': "application/x-www-form-urlencoded",
+'Cache-Control': "no-cache",
+}
+response = requests.request("POST", url, data=payload, headers=headers)
+print(response.text)
+
